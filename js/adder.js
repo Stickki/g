73525d6f0cbe613529883206c9d0b548ac9bc58a
Tumko Ivan <114734820+Ivan-blep@users.noreply.github.com>
2023-03-16 00:27:23 +0300

    Add files via upload

--- js/adder.js
+++ js/adder.js
@@ -1,23 +1,6 @@
 // import items from './db.json' assert {type: 'json'};
 
-function showGroup(group) {
-  document.querySelector(".inputBoxList").value = group;
-}
-function showSurname(surname) {
-  document.querySelector(".boxName").value = surname;
-}
 
-let dropdown = document.querySelector(".groupList1");
-let dropactive = document.querySelector(".dropBlockList1");
-dropdown.onclick = function () {
-  dropactive.classList.toggle("active");
-};
-
-let dropdown2 = document.querySelector(".groupList2");
-let dropactive2 = document.querySelector(".dropBlockList2");
-dropdown2.onclick = function () {
-  dropactive2.classList.toggle("active");
-};
 
 let settingsIcon = document.querySelector(".settingsIcon");
 let settings = document.querySelector(".settings");
@@ -27,22 +10,10 @@ settingsIcon.onclick = function () {
   containerSettings.classList.toggle("active");
 };
 
-const inputBoxList = document.querySelector(".inputBoxList");
-const dropListElement = document.querySelectorAll(".dropListElement");
-const boxName = document.querySelector(".boxName");
 
 
-inputBoxList.addEventListener("click", () => {
-  inputBoxList.classList.toggle("inputClick");
-});
-boxName.addEventListener("click", () => {
-  boxName.classList.toggle("inputClick");
-});
-dropListElement.forEach((elem) => {
-  elem.addEventListener("click", () => {
-    inputBoxList.classList.remove("inputClick");
-  });
-});
+
+
 
 const withdrawalCard = () => {
 
@@ -75,29 +46,6 @@ const withdrawalCard = () => {
 
 document.querySelector("#acceptButton").onclick = withdrawalCard;
 
-const dropBlockList2 = async () => {
-  let response = await fetch('js/surnames.json');
-  let arr = await response.json();
-  let dropBlockList2 = document.querySelector('.dropBlockList2');
-  let listElements = dropBlockList2.querySelectorAll('.dropListElement');
-
-  // Проверяем наличие элементов в списке
-  if (listElements.length === 0) {
-    for (let i = 0; i < arr.length; i++) {
-      let divList = document.createElement('div');
-      divList.classList.add('dropListElement');
-      divList.innerHTML = `${arr[i]}`;
-      divList.addEventListener('click', () => {
-        showSurname(arr[i]);
-      });
-      dropBlockList2.append(divList);
-    }
-  }
-};
-
-if (document.querySelector("#surname")) {
-  document.querySelector("#surname").onclick = dropBlockList2;
-}
 
 
 // картинка фону прикладу
